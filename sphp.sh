3a9d6184f2d11c9bbcb63801aa3b3abc15ff1829
Andy Miller <1084697+rhukster@users.noreply.github.com>
2023-10-20 11:03:50 +0100

    added 8.3 and 8.4 support

--- sphp.sh
+++ sphp.sh
@@ -13,7 +13,7 @@
 script_version=1.1.0
 
 # Supported PHP versions
-brew_array=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2")
+brew_array=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2" "8.3" "8.4")
 
 # Apache configuration switch: 1=enabled, 0=disabled
 apache_change=1
